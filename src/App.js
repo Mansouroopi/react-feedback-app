efce0e22295ce0f9f1874b4cfa38e0685d7216ba
Mansour <mansour.coder@gmail.com>
2021-12-31 08:23:40 +0800

    end of props drilling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
-import { useState } from "react"
 import { v4 as uuidv4 } from "uuid"
+import AboutPage from "./pages/AboutPage"
+import AboutIconLink from "./components/AboutIconLink"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -22,14 +25,30 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header />
+
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={handleFeedback} />
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={handleFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   )
 }
 
